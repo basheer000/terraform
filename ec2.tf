4a7ea0211e1412fe2fd1985c6574f3f2b994e78f
basheerahammad <skbasheerahammad666@gmail.com>
2022-11-05 18:01:39 +0530

    instnace

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,11 @@
 resource "aws_instance" "tf-inst" {
     ami = "ami-09d3b3274b6c5d4aa"
     instance_type = "t2.micro"
+    ebs_block_device {
+      volume_type = "30"
+      volume_size = "gp2"
+      device_name = "/dev/xvdf"
+    }
     tags = {
       "Name" = "tf-instance"
     }
